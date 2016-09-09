4ea5da9635fa0d590e62f16274e7084103cdb1c0
naveenvpai <pai.naveen357@gmail.com>
2016-09-09 18:03:51 +0000

    Establish User-Post relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   validates :name, presence: true;
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create };
 end
