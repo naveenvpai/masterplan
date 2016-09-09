dfb23fc31a0b33ea65bdc5a834370d5d7f835b02
naveenvpai <pai.naveen357@gmail.com>
2016-09-09 17:14:45 +0000

    Scaffold a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "shoot a monkey?"
+    render html: "Lets make some users."
   end
 end
