dbd45f56322736e85b13798856e08eeb14658206
naveenvpai <pai.naveen357@gmail.com>
2016-09-26 17:22:58 +0000

    Update user test suite

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,11 +25,19 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
-  test "emails should be valid" do
+  test "emails should be accepted" do
     valids = %w[user@example.com USER@foo.com A_USER-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valids.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{@user.email} should be valid"
+    end
+  end
+  
+  test "emails should be rejected" do
+    invalids = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalids.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{@user.email} should be invalid"
     end
   end
   
