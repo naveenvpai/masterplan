6b9554e37bfc78af650099a589e6d7afb6961fe6
naveenvpai <pai.naveen357@gmail.com>
2016-11-14 19:28:44 +0000

    Add conditional logout link

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,4 +5,13 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get login_path
     assert_response :success
   end
+  
+  test "stays logged in" do
+    get login_path
+    post login_path, params: { session: { email:users(:michael).email, password:"password" } }
+    assert_redirected_to users(:michael)
+    get root_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+  end
 end
