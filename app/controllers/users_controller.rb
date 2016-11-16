23c422546b0b6fdf69145da5ae0e52dbdc6a1855
naveenvpai <pai.naveen357@gmail.com>
2016-11-16 19:19:26 +0000

    Log in user after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
+      log_in @user
     else
       render 'new'
     end
