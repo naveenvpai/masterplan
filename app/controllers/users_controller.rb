ec48bd57217f858194f15ed2e1de2919a2530644
naveenvpai <pai.naveen357@gmail.com>
2016-12-12 18:58:01 +0000

    Authorize user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:show];
+  before_action :correct_user, only: [:show];
+
   def new
     @user = User.new
   end
   
   def show
-    @user = User.find(params[:id])
     @photos = @user.photos
   end
   
@@ -24,6 +25,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to root_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
 end
 
