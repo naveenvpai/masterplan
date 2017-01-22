df743501912224a224db22209c89be0fc500ed9f
naveenvpai <pai.naveen357@gmail.com>
2017-01-21 17:24:27 -0800

    Add sessions helper comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  //make methods such as logged_in? accessible for controller validation
   include SessionsHelper
 end
