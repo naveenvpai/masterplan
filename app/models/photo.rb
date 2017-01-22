d4c2812f84ef67199872d4558c2a2f5a9032f759
naveenvpai <pai.naveen357@gmail.com>
2017-01-21 17:49:27 -0800

    Clarify photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
+  #Signals to the Paperclip framework that photo upload functionality is required for this object
   has_attached_file :image
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
   belongs_to :user
